115a7106b0de4b73a901cdf3d38ef6920ede19c2
Kavia AI <production@kavia.ai>
2025-09-09 17:23:21 +0000

    CheckPoint - cg21f3861c

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/nurseconnect-59428-35759/nurse_hospital_frontend
+flutter analyze
+
---
+++ nurse_hospital_frontend/android/app/src/main/kotlin/com/example/nurse_hospital_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nurse_hospital_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
